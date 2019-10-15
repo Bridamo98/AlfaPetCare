40126102a30351e719e94557cf2aabd64f96d769
Bridamo98 <bridamo.98@gmail.com>
2019-10-15 18:09:10 +0000

    falta confugurar archivos estáticos

--- APC/APC/settings.py
+++ APC/APC/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b!l(yg7mjou@bred8v%fn=j4rkzk$*et7pys1i6g%+1%@!w+6='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alfapetcare.pythonanywhere.com']
 
 
 # Application definition
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'APC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
