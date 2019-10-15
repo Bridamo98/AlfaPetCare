4919c58d4bac7a29758fe55371c6879c6c816a4b
Briam Daniel Agudelo Mogollón <bridamo.98@gmail.com>
2019-10-15 13:37:03 -0500

    conf static files

--- APC/APC/settings.py
+++ APC/APC/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b!l(yg7mjou@bred8v%fn=j4rkzk$*et7pys1i6g%+1%@!w+6='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alfapetcare.pythonanywhere.com']
+ALLOWED_HOSTS = ['alfapetcare.pythonanywhere.com','localhost']
 
 
 # Application definition
@@ -124,4 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
