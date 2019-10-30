ef2593f630cb60044907afa6e8e552acfa5bb67b
Oscar Fonseca <fonseca.oscar@javeriana.edu.co>
2019-10-30 14:05:51 -0500

    Registro, login y logout

--- APC/apps/gestor_de_usuarios/views.py
+++ APC/apps/gestor_de_usuarios/views.py
@@ -7,11 +7,18 @@ from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from .forms import FormularioLogin
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+"""
+Renderiza el template index o página de inicio
+"""
 def Home(request):
     return render(request,'index.html')#HTTP request
 #end def
-
+"""
+Valida el login de un usuario
+"""
 class Login(FormView):
     template_name = 'login.html'
     form_class = FormularioLogin
@@ -32,12 +39,25 @@ class Login(FormView):
         login(self.request,form.get_user())
         return super(Login,self).form_valid(form)
 #end class
-
+"""
+Efectua el logout de un usuario
+"""
 def Logout(request):
     logout(request)
     return HttpResponseRedirect('/accounts/login')
 #end def
 
+
+"""
+Valida y renderiza el registro de un usuario
+"""
 def Registro(request):
-    return render(request,'registro.html')#HTTP request
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,'registro.html',{'form':form})
 #end def
